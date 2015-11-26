cbc10d23673d71aa092800e1fa6050a4ed684839
honorabrutroll <edward.yang6771@gmail.com>
2015-11-26 02:43:53 -0600

    Finish windows build script

--- build.rs
+++ build.rs
@@ -1,16 +1,21 @@
+use std::env;
+use std::fs::copy;
+
 fn main() {
-    //get_dep_dir();
     if cfg!(windows) {
+        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let outdir = env::var("OUT_DIR").unwrap();
         let depdir; 
         if cfg!(target_pointer_width = "64") {
             //64 bit
-            depdir = "./windows-deps/x86_64";
+            depdir = format!("{}{}", manifest_dir, "\\windows-deps\\x86_64");
         } else {
             //32 bit
-            depdir = "./windows-deps/x86";
-        }  
+            depdir = format!("{}{}", manifest_dir, "\\windows-deps\\x86");
+        }
         println!("cargo:rustc-link-search={}", depdir);
-    } else { 
-        //do nothing
+        println!("{}", outdir);
+        copy(format!("{}{}", manifest_dir, "\\windows-deps\\pbook-gui.exe.manifest"), format!("{}{}", outdir, "\\..\\..\\..\\pbook-gui.exe.manifest")).expect("Failed to copy manifest");
+        copy(format!("{}{}", depdir, "\\iup.dll"), format!("{}{}", outdir, "\\..\\..\\..\\iup.dll")).expect("Failed to copy iup.dll");
     }
 }
