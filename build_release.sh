1bd675ae3846bad2cc9d52d1ea2da850c00c3485
honorabrutroll <edward.yang6771@gmail.com>
2015-11-26 10:29:26 -0600

    Fix build scripts, add 7z

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,8 @@
 cargo build --release
 cd "target/release"
-cp "pbook-gui" "pbook-guic"
+mv "pbook-gui" "pbook-guic"
+mkdir "pbook-gui"
+cp "pbook-guic" "pbook-gui/pbook-gui"
 tar cfJ "pbook-gui.tar.xz" "pbook-gui"
+rm -rf "pbook-gui"
 mv "pbook-guic" "pbook-gui"
