70696d896aaff024f5bceceaf2b08ac36c949709
Edward Yang <edward.yang6771@gmail.com>
2016-03-16 08:44:17 -0500

    More work on fsthread

--- src/fsthread.rs
+++ src/fsthread.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 use std::fs;
 use std::thread;
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::io;
 
 pub enum FsCommand {
     Copy(PathBuf, PathBuf), // source, dest
@@ -10,24 +11,42 @@ pub enum FsCommand {
 }
 
 pub enum FsUpdate {
-    Failed(String),
+    Error(String),
 }
 
 pub struct FsThread {
-    command_recv: Receiver<FsCommand>,
-    update_send: Sender<FsUpdate>,
+    pub command_recv: Receiver<FsCommand>,
+    pub update_send: Sender<FsUpdate>,
 }
 
 impl FsThread {
+    fn new(command_recv: Receiver<FsCommand>, update_send: Sender<FsUpdate>) -> FsThread {
+        FsThread {
+            command_recv: command_recv,
+            update_send: update_send,
+        }
+    }
+
     pub fn spawn() -> (Sender<FsCommand>, Receiver<FsUpdate>) {
         unimplemented!();
+        let (update_send, update_recv) = channel();
+        let (command_send, command_recv) = channel();
+        let builder = thread::Builder::new();
+        builder.stack_size(100000);
+        builder.name("File System Thread".to_owned());
+        builder.spawn(move || {
+            let run = true;
+            let fsthread = FsThread::new(command_recv, update_send);
+            while run {
+                match fsthread.command_recv.recv() {
+                    
+                }
+            }
+        });
+        return (command_send, update_recv);
     }
 
-    fn update(&mut self) -> Result<(), String> {
-        unimplemented!();
-    }
-
-    fn handle_command(&mut self, command: FsCommand) {
+    fn handle_command(&self, command: FsCommand) {
         unimplemented!();
     }
 }
